58c32cb1dd7dd4cf9ac03901127454d2fb8a0779
Dmytro Sayko <saykodmytro@icloud.com>
2023-09-26 13:07:36 +0300

    up

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/by-now-modal';
+import './js/explore-chocolate-modal';
+import './js/leave-a-review-modal';
+import './js/mobile-menu';
+import './js/our-products-slider';
+import './js/reviews-slider';
+import './js/phone-code-select';
